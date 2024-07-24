5c030d71022644a55a9833fd4b5d41fa5fad562f
gwynethtan <S10257317@connect.np.edu.sg>
2024-07-24 23:52:33 +0800

    Dating profiles can accept profile number inputs
    Profile data is more detailed and can store chosen dating profile data

---
+++ Assets/Scripts/DatingProfiles.cs
@@ -0,0 +1,27 @@
+/*
+* Author: Tan Ting Yu Gwyneth 
+* Date: 24/07/2024
+* Description: This file is for handling player interactions like rays and button input 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class DatingProfiles : MonoBehaviour
+{
+    //hv to store profile detail in game manager
+    //robot need to appear in game manager too for it to not disappear
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
